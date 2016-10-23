c7f4c872e8a91c4b173fd06182efbe5454d93877
drzackyll <adams.z.d@gmail.com>
2016-10-23 17:37:01 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.create(name: "Zack Adams", bio: "Born in a bathtub")
+Artist.create(name: "Bruce Springsteen", bio: "Born to run")
+Artist.create(name: "Lauren Adams", bio: "Also born in a bathtub")
